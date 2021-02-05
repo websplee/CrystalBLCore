a72bb0e9f7a66bea8d6f27d7737f081c7e52603d
Humphrey Chinyama <hmpsplee@gmail.com>
2021-02-05 04:34:43 +0200

    BL objects email DL custom exception

---
+++ CrystalBLCore/BusinessServices/EmailService.cs
@@ -0,0 +1,125 @@
+﻿using CrystalBLCore.BusinessServices.Interfaces;
+using CrystalBLCore.Models;
+using CrystalBLCore.Models.Interfaces;
+using MailKit.Net.Pop3;
+using MailKit.Net.Smtp;
+using MimeKit;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrystalBLCore.BusinessServices
+{
+    public class EmailService : IEmailService
+    {
+        public readonly IEmailConfiguration _emailConfiguration;
+
+        public EmailService(IEmailConfiguration emailConfiguration)
+        {
+            _emailConfiguration = emailConfiguration;
+        }
+
+        public List<EmailMessage> ReceiveEmail(int maxCount = 10)
+        {
+            using (var emailClient = new Pop3Client())
+            {
+                emailClient.Connect(_emailConfiguration.PopServer, _emailConfiguration.PopPort, true);
+
+                emailClient.AuthenticationMechanisms.Remove("XOAUTH2");
+
+                emailClient.Authenticate(_emailConfiguration.PopUsername, _emailConfiguration.PopPassword);
+
+                List<EmailMessage> emails = new List<EmailMessage>();
+                for (int i = 0; i < emailClient.Count && i < maxCount; i++)
+                {
+                    var message = emailClient.GetMessage(i);
+                    var emailMessage = new EmailMessage
+                    {
+                        Content = !string.IsNullOrEmpty(message.HtmlBody) ? message.HtmlBody : message.TextBody,
+                        Subject = message.Subject
+                    };
+                    emailMessage.ToAddresses.AddRange(message.To.Select(x => (MailboxAddress)x).Select(x => new EmailAddress { Address = x.Address, Name = x.Name }));
+                    emailMessage.FromAddresses.AddRange(message.From.Select(x => (MailboxAddress)x).Select(x => new EmailAddress { Address = x.Address, Name = x.Name }));
+                    emails.Add(emailMessage);
+                }
+
+                return emails;
+            }
+        }
+
+        public async Task Send(EmailMessage emailMessage)
+        {
+            // Compose a message
+            MimeMessage mail = new MimeMessage();
+            //mail.From.Add(new MailboxAddress("Inventory Control System", "hchinyama@crystalisedliquid.com"));
+            mail.From.Add(new MailboxAddress(emailMessage.FromAddresses[0].Name, emailMessage.FromAddresses[0].Address));
+            //mail.To.Add(new MailboxAddress("H Gmail", "hmsplee@gmail.com"));
+            mail.To.Add(new MailboxAddress(emailMessage.ToAddresses[0].Name, emailMessage.ToAddresses[0].Address));
+            //mail.To.Add(new MailboxAddress("H Gmail", "cynthiambulo@gmail.com"));
+            //mail.Subject = "Mail";
+            mail.Subject = emailMessage.Subject;
+            mail.Body = new TextPart("html")
+            {
+                Text = emailMessage.Content,
+            };
+            using (var emailClient = new SmtpClient())
+            {
+                emailClient.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+                //The last parameter here is to use SSL (Which you should!)
+                await emailClient.ConnectAsync(_emailConfiguration.SmtpServer, _emailConfiguration.SmtpPort, false);
+
+                //Remove any OAuth functionality as we won't be using it. 
+                emailClient.AuthenticationMechanisms.Remove("XOAUTH2");
+
+                await emailClient.AuthenticateAsync(_emailConfiguration.SmtpUsername, _emailConfiguration.SmtpPassword);
+
+                await emailClient.SendAsync(mail);
+
+                await emailClient.DisconnectAsync(true);
+            }
+            /**
+            // Send it!
+            using (var client = new SmtpClient())
+            {
+                // XXX - Should this be a little different?
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+                client.Connect("smtp.mailgun.org", 587, false);
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+                client.Authenticate("postmaster@sandboxa6cd16bff0d447e1877a8a05701c3b9f.mailgun.org", "d58be90e305b079fe0f799adb834fabf-c27bf672-67a5076e");
+
+                client.Send(mail);
+                client.Disconnect(true);
+            }
+
+
+            /*var message = new MimeMessage();
+            message.To.AddRange(emailMessage.ToAddresses.Select(x => new MailboxAddress(x.Name, x.Address)));
+            message.From.AddRange(emailMessage.FromAddresses.Select(x => new MailboxAddress(x.Name, x.Address)));
+
+            message.Subject = emailMessage.Subject;
+            //We will say we are sending HTML. But there are options for plaintext etc. 
+            message.Body = new TextPart(TextFormat.Html)
+            {
+                Text = emailMessage.Content
+            };
+
+            //Be careful that the SmtpClient class is the one from Mailkit not the framework!
+            using (var emailClient = new SmtpClient())
+            {
+                //The last parameter here is to use SSL (Which you should!)
+                emailClient.Connect(_emailConfiguration.SmtpServer, _emailConfiguration.SmtpPort, true);
+
+                //Remove any OAuth functionality as we won't be using it. 
+                emailClient.AuthenticationMechanisms.Remove("XOAUTH2");
+
+                emailClient.Authenticate(_emailConfiguration.SmtpUsername, _emailConfiguration.SmtpPassword);
+
+                emailClient.Send(message);
+
+                emailClient.Disconnect(true);
+            }*/
+        }
+    }
+}
